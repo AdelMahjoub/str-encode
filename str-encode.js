cabde5a6c073bdc20033de2d223a015beaab4671
Adel Mahjoub <dragonhunter250@gmail.com>
2017-06-16 02:08:25 +0200

    updated

--- str-encode.js
+++ str-encode.js
@@ -6,11 +6,9 @@ const buffer = require('buffer');
  * @param { string to encode} str 
  * @param { encoding output} encoding 
  */
-const strEncode = function(str, encoding) {
+module.exports = function(str, encoding) {
   if(!Buffer.isEncoding(encoding.toString())) {
     throw Error(util.format("cannot encode to", encoding));
   }
   return Buffer.from(str.toString()).toString(encoding);
 }
-
-module.exports = strEncode;
